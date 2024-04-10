e33622bee81cff9f3cb4001bab437961f3c267c8
Shubham Shelar <85821105+shubhamshelar@users.noreply.github.com>
2024-04-10 22:25:48 +0530

    defined structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './screens/registration/registration.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './screens/login/login.component';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { LoaderComponent } from './shared/component/loader/loader.component';
+import { ModalComponent } from './shared/component/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     
     RegistrationComponent,
-         LoginComponent
+         LoginComponent,
+         HeaderComponent,
+         FooterComponent,
+         LoaderComponent,
+         ModalComponent
   ],
   imports: [
     BrowserModule,
