f890e4d688c6d39e29768fa2f3115f47a11d3e9e
Harshwardhan Atkare <81025973+Harshwardhan431@users.noreply.github.com>
2024-04-12 13:30:56 +0530

    ---

--- src/app/screens/enrolled-courses/enrolled-courses.component.ts
+++ src/app/screens/enrolled-courses/enrolled-courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from 'src/app/service/course.service';
 import { Course } from 'src/app/model/course.model';
+import { StudentService } from 'src/app/service/student.service';
 
 @Component({
   selector: 'app-enrolled-courses',
@@ -11,15 +12,16 @@ export class EnrolledCoursesComponent implements OnInit {
 
   
 
-  constructor(public es:CourseService) { 
+  constructor(public es:CourseService,public ss:StudentService) { 
     
   }
 
   ngOnInit(): void {
-    this.refereshCourses();
+    this.refereshAllCourses();
+    this.getEnrolledCourses();
   }
 
-  refereshCourses()
+  refereshAllCourses()
   {
     this.es.getAllCourses().subscribe((res)=>{
       this.es.allCourse=res as Course[];
@@ -27,5 +29,12 @@ export class EnrolledCoursesComponent implements OnInit {
     })
   }
 
+  getEnrolledCourses()
+  {
+    this.es.getAllEnrolledCourses(this.ss.student.stud_id).subscribe((res)=>{
+      this.es.enrolledCourses=res as Course[];
+      console.log(this.es.enrolledCourses)
+    })
+  }
 
 }
