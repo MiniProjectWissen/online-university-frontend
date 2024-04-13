97c2e631b831665bcfbb61a3985f53094da1332a
RushikeshKhiste <rushikeshkhiste2002@gmail.com>
2024-04-13 22:09:37 +0530

    Course list by teacher added

---
+++ src/app/screens/my-courses/my-courses.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NumberValueAccessor } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Course } from 'src/app/model/course.model';
+import { CourseService } from 'src/app/service/course.service';
+import { TeacherService } from 'src/app/service/teacher.service';
+
+@Component({
+  selector: 'app-my-courses',
+  templateUrl: './my-courses.component.html',
+  styleUrls: ['./my-courses.component.css']
+})
+export class MyCoursesComponent implements OnInit {
+  myCourseSubscription: Subscription
+  myCourseList : Course[];
+
+  constructor(public cs:CourseService,public ts:TeacherService) {
+    this.myCourseSubscription = new Subscription();
+    this.myCourseList = [];
+   }
+
+  ngOnInit(): void {
+    this.loadTeacherCourses();
+  }
+
+  loadTeacherCourses(){
+    let teacherId = this.ts.teacher.teacher_id;
+    console.log(teacherId)
+    this.myCourseSubscription = this.cs.getAllCoursesByTeacher(teacherId).subscribe(
+      (courses:Course[])=>{
+        this.myCourseList = courses;
+        console.log("Recieved course list by teacher "+courses)
+      }
+    )
+
+  }
+}
